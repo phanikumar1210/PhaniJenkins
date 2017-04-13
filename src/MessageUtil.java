50eacbc20b208f0540d6a51ec8f7334ba00c8940
phanikumar1210 <phanikumardadi.kumar@gmail.com>
2017-04-14 03:25:46 +0530

    Sample

--- src/MessageUtil.java
+++ src/MessageUtil.java
@@ -12,7 +12,7 @@ public class MessageUtil {
    }
    // add "Hi!" to the message
    public String salutationMessage(){
-      message = "Hi!a1" + message;
+      message = "Hi!" + message;
       System.out.println(message);
       return message;
    }   
